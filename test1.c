bb5b99cb8328ea40873f46dc02d7f517a110bbc0
FlagIX <2689729698@qq.com>
2023-09-09 09:00:58 +0800

    hello world

---
+++ test1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int test1() {
+    char a = 'x';
+    char b = 120;
+    printf("a = %c\n", a);
+    printf("b = %c\n", b);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
