6fb2a9c59733f9e529e29ae81db31d6f9420d133
FlagIX <2689729698@qq.com>
2023-09-10 14:11:34 +0800

    %d

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+//《学习c语言，从入门到入土》
+
+
 #include <stdio.h>
 #include "test1.h"
 int main()
@@ -6,9 +9,31 @@ int main()
     int b = 666;
     float c = 1.144f;
     double d = 1.56;
+    int e = 0x5e;
+    float f = 0.5464f;
+    double g = 1350437095;
+    char* p = "易子建傻逼";
     printf("a =%c\n",a);
     printf("b =%d\n",b);
     printf("c=%f\n",c);
     printf("d=%lf\n",d);
+    printf("e+%x\n",e);
+    printf("e=%d\n",e);
+    printf("f=%e\n",f);
+    printf("g=%e\n",g);
+    printf("%s\n",p);
+
+
+
+    printf("**%4d**\n",558);
+    printf("**%03d**\n",1);
+    printf("**%-3d**\n",1);
+    printf("**%10.2f**\n",551.556);//   .几就是保留几位小数（带四舍五入）
+    //5就是小数点前的5位数，少于5则会空出来，多余5则不起作用
+
+
+    printf("hello world\n");
+    char* yzk = "易子康";
+    printf("yzk=%s\n",yzk);
     return 0;
 }
--- test1.c
+++ test1.c
@@ -1,28 +1,24 @@
-#include <stdio.h>
-#include "test1.h"
-
-int test1() {
-    char a = 'x';
-    char b = 120;
-    printf("a = %c\n", a);
-    printf("b = %c\n", b);
-    return 0;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//#include <stdio.h>
+//#include "test1.h"
+//int test1()
+//{
+//    printf("易子建傻逼");
+//    return 0;
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
